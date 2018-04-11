3a8479df85df5bc38ff7ec7166355a510b3fc769
GABRIEL ROBERTO FEITOSA DA COSTA <gabriel.rfcosta1@senacsp.edu.br>
2018-04-11 08:54:54 -0300

    Declaração de variaveis

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ namespace PrimeiroGit
     {
         static void Main(string[] args)
         {
+         //Declaração das variaveis para guardar os valores
+         //que o usuario entrará
+         Console.Clear();
+         double valor1,valor2,resultado;
             
         }
     }
